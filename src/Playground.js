62da810597301e408321732d67ad9e0b81b4a895
arxk0430 <ky1106199942@gmail.com>
2023-06-29 02:37:03 +0800

    ready for deploy

--- src/Playground.js
+++ src/Playground.js
@@ -4,30 +4,16 @@ import Dodecahedron from './Dodecahedron';
 import {SharpIcosahedron, Icosahedron} from './Icosahedron';
 import Cube from './Cube';
 
-const Boxes = () => {
-    return(
-        <div style={{marginBottom: "440px"}}>
-            <div className="box" style={{animation: "moving-box 4s ease-in-out forwards infinite"}}/>
-            <div className="box" style={{animation: "moving-box 4s ease-in-out forwards infinite -1s"}}/>
-            <div className="box" style={{animation: "moving-box 4s ease-in-out forwards infinite -2s"}}/>
-            <div className="box" style={{animation: "moving-box 4s ease-in-out forwards infinite -3s"}}/>
-        </div>
-    )
-}
-
 const Playground = () => {
     return(
         <div className="body">
-            <SharpIcosahedron />
+            <h1>Tried to draw some polyhedrons with React.js and SASS. Nice mathematics exercise.</h1>
+            {/* <SharpIcosahedron /> */}
             <TriangularBipyramid />
             <Icosahedron />
             <Dodecahedron />
             <Tetrahedron />
             <Cube />
-            <div className="parent">
-                <div className="child" />
-            </div>
-            <Boxes />
         </div>
     )
 }
